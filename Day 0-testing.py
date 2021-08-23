92ea0036898875207c583e516c59aba5bc39a60f
William Li <billgan.lee@gmail.com>
2021-08-22 21:06:55 -0700

    CS 162 预习

--- Day 0-testing.py
+++ Day 0-testing.py
@@ -1,3 +1,16 @@
 # Author: Gan Li
 # Date: 8/22/21 7:52 下午
 # Description: 随便测试的内容
+def right_justify(s):
+    """3.14 exercise 1"""
+    length = len(s)
+    return ' ' * (70 - length) + s
+
+
+def main():
+    string = input('Please type in a string less than 70 characters:')
+    print(right_justify(string))
+
+
+if __name__ == "__main__":
+    main()
