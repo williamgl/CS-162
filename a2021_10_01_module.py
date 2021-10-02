e666b52ce7618588395f92eb278527378f61dd67
William Li <billgan.lee@gmail.com>
2021-10-01 18:01:55 -0700

    2021/10/01 module 3

--- a2021_10_01_module.py
+++ a2021_10_01_module.py
@@ -1,10 +1,66 @@
 # Author: Gan Li
 # Date: 10/1/21 12:45 下午
 # Description: Module 3 inheritance, composition, and polymorphism
+import random
+
+
 class Card:
     """Represents a standard playing card."""
-
     def __init__(self, suit=0, rank=2):
         self.suit = suit
         self.rank = rank
 
+    suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+    rank_names = [None, 'Ace', '2', '3', '4', '5', '6', '7',
+                  '8', '9', '10', 'Jack', 'Queen', 'King']
+
+    def __str__(self):
+        return '%s of %s' % (Card.rank_names[self.rank],
+                             Card.suit_names[self.suit])
+
+    def __lt__(self, other):
+        card_1 = self.suit, self.rank
+        card_2 = other.suit, other.rank
+        return card_1 < card_2
+
+
+class Deck:
+    """Represents a standard deck of 52 cards."""
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
+
+    def __str__(self):
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return '\n'.join(res)
+
+    def pop_card(self):
+        return self.cards.pop()
+
+    def remove_card(self, card):
+        for card_sample in self.cards:
+            if str(card) == str(card_sample):
+                self.cards.pop(self.cards.index(card_sample))
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def move_cards(self, hand, num):
+        for i in range(num):
+            hand.add_card(self.pop_card())
+
+
+class Hand(Deck):
+    """Represents a hand of playing cards."""
+    def __init__(self, label=''):
+        super().__init__()  # 没有也没关系
+        self.cards = []
+        self.label = label
--- test03.py
+++ test03.py
@@ -1,3 +1,37 @@
 # Author: Gan Li
 # Date: 10/1/21 12:54 下午
 # Description:
+class Bird:
+    def __init__(self):
+        pass
+
+    def call(self):
+        return 'chirp'
+
+
+class Duck(Bird):
+    def __init__(self):
+        super().__init__()
+
+    def call(self):
+        return 'quack'
+
+
+class Employee:
+    def __init__(self, name, salary):
+        self._name = name
+        self._salary = salary
+
+    def get_name(self):
+        return self._name
+
+    def get_salary(self):
+        return self._salary
+
+    def gross_monthly_pay(self):
+        return self._salary / 12
+
+
+class SeniorEmployee(Employee):
+    def gross_monthly_pay(self):
+        return super().gross_monthly_pay() + 500
